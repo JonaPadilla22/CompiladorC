9cf3ee63c25bd153a6e5780da9607c1b418db9e6
jona22 <jonathan_pad22@hotmail.com>
2022-09-30 00:35:38 -0600

    lexico con lista enlazada

--- main.c
+++ main.c
@@ -2,12 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <conio.h>
 #include "automataDecimal.h"
-#include "automataSimbolos.h"
 #include "automataCadenas.h"
 
 char *simbolos = "<>$=!)(}{:\";+-*/\\";
 char caracter,*ret;
+int num_lin = 1;
+long col_ac = 0;
+const char* tipoTokenNames[] = {"PALABRA RESERVADA", "ID", "NUMERO", "SIMBOLO", "CADENA"};
+
+
+enum TipoToken
+{
+    PalRes,
+    id,
+    num,
+    sim,
+    cad
+};
+
+struct Token
+{
+    //char Nombre[80];
+    enum TipoToken Tipo;
+    char *Lexema;
+    int Valor;
+    int NumLinea;
+};
+
+struct nodo
+{
+    struct Token info;
+    struct nodo *izq;
+    struct nodo *der;
+};
+
+struct nodo *raiz;
+struct nodo *actual;
+
+void insertar(struct Token token)
+{
+    struct nodo *nuevo;
+    nuevo = malloc(sizeof(struct nodo));
+
+    nuevo->info = token;
+    nuevo->izq = NULL;
+    nuevo->der = NULL;
+
+    if (raiz == NULL)
+    {
+        raiz = nuevo;
+        actual = nuevo;
+    }
+    else
+    {
+        nuevo->izq = actual;
+        actual->der = nuevo;
+        actual = nuevo;
+    }
+}
+
+void imprimirlista(struct nodo *inicio){
+    printf("\n------------LISTA ENLAZADA------------\n");
+    struct nodo *act;
+    act=inicio;
+    int elem = 0;
+    while(act!=NULL){
+        printf("\nELEMENTO %d\n", elem),
+        printf("Tipo: %s\n", tipoTokenNames[act->info.Tipo]);
+        printf("Lexema: %s\n", act->info.Lexema);
+        printf("Valor: %d\n", act->info.Valor);
+        printf("Linea: %d\n", act->info.NumLinea);
+        act=act->der;
+        elem++;
+    }
+}
 
 int isSimbol(char c){
 
@@ -172,18 +243,20 @@ int isIdentificador (FILE* punt) {
 
 }
 
-void encontrarEspacio(FILE* punt) {
-    char c = caracter;
-    while (c != ' ') {
-        c = (char)fgetc(punt);
-        printf("%c", c);
-    }
+void contarLinea(FILE* file){
+    num_lin++;
+    printf("Linea %d\n", num_lin);
+    //caracter = (char)fgetc(file);
+    col_ac = ftell(file) - 1;
 }
 
-void encontrarFinLinea(FILE* punt) {
+void encontrarEspacio(FILE* punt) {
     char c = caracter;
-    while (c != '\n') {
+    while (c != ' ' && c != '\n') {
         c = (char)fgetc(punt);
+    }
+    if(c == '\n'){
+        contarLinea(punt);
     }
 }
 
@@ -204,7 +277,6 @@ char *lexema (FILE* file, int aux, int act) {
 int main(){
     FILE* file;
     int aux;
-    int num_lin = 1;
     char* lex;
     char* a;
     lex = (char*)malloc(sizeof(char));
@@ -228,6 +300,14 @@ int main(){
 
                 lex = lexema(file, aux, ftell(file));
                 printf(" --- lexema: %s\n", lex);
+
+                struct Token t;
+                t.Lexema = lex;
+                t.NumLinea = num_lin;
+                t.Valor = atoi(lex);
+                t.Tipo = num;
+                insertar(t);
+
             }else{
                 //si no lo aceptó el automata, se devuelve el puntero a donde estaba antes de entrar al mismo
                 fseek(file, aux, SEEK_SET);
@@ -237,10 +317,17 @@ int main(){
 
                     lex = lexema(file, aux, ftell(file));
                     printf(" --- lexema: %s\n", lex);
+
+                    struct Token t;
+                    t.Lexema = lex;
+                    t.NumLinea = num_lin;
+                    t.Tipo = num;
+                    t.Valor = atof(lex);
+                    insertar(t);
                 }else{
 
                     fseek(file, aux, SEEK_SET);
-                     caracter = (char)fgetc(file);
+                    caracter = (char)fgetc(file);
                     //si tampoco se manda al de simbolo
                     if(isSimbol(caracter)!=0){
 
@@ -250,16 +337,35 @@ int main(){
                                 printf("es cadena");
                                 lex = lexema(file, aux, ftell(file)+1);
                                 printf(" --- lexema: %s\n", lex);
+
+                                struct Token t;
+                                t.Lexema = lex;
+                                t.NumLinea = num_lin;
+                                t.Tipo = cad;
+                                t.Valor = 0;
+                                insertar(t);
                             }else{
-                                printf("error lexico \n");
+                                int col_err = aux - col_ac + 1;
+                                printf("error lexico en la columna %d\n", col_err);
                                 fseek(file, -1, SEEK_CUR);
                             }
                         }else{
                             printf("es simbolo");
                             lex = lexema(file, aux, ftell(file)+1);
                             printf(" --- lexema: %s\n", lex);
+
+                            struct Token t;
+                            t.Lexema = lex;
+                            t.NumLinea = num_lin;
+                            t.Tipo = sim;
+                            t.Valor = 0;
+                            insertar(t);
                         }
 
+                    }else{
+                        int col_err = aux - col_ac + 1;
+                        printf("error lexico en la columna %d\n", col_err);
+                        encontrarEspacio(file);
                     }
 
 
@@ -272,18 +378,27 @@ int main(){
             //else if --- si es letra mandar a de palabra reservada, dentro de (si no es) mandar al de indentificador
             if(isIdentificador(file) == 1){
                 lex = lexema(file, aux, ftell(file));
-
+                struct Token t;
                 if(isPalabraReservada(lex) == 1){
                     printf("es palabra reservada");
+                    t.Tipo = PalRes;
                 }else{
                     printf("es identificador");
+                    t.Tipo = id;
                 }
 
                 printf(" --- lexema: %s\n", lex);
 
+
+                t.Lexema = lex;
+                t.NumLinea = num_lin;
+                t.Valor = 0;
+                insertar(t);
             }else{
-                printf("error lexico \n");
+                int col_err = aux - col_ac + 1;
+                printf("error lexico en la columna %d\n", col_err);
                 encontrarEspacio(file);
+                //fseek(file, 1, SEEK_CUR);
             }
 
         }//si es simbolo mandar al de simbolo
@@ -295,14 +410,29 @@ int main(){
                     printf("es cadena");
                     lex = lexema(file, aux, ftell(file)+1);
                     printf(" --- lexema: %s\n", lex);
+
+                    struct Token t;
+                    t.Lexema = lex;
+                    t.NumLinea = num_lin;
+                    t.Tipo = cad;
+                    t.Valor = 0;
+                    insertar(t);
                 }else{
-                    printf("error lexico \n");
+                    int col_err = aux - col_ac + 1;
+                    printf("error lexico en la columna %d\n", col_err);
                     fseek(file, -1, SEEK_CUR);
                 }
             }else{
                 printf("es simbolo");
                 lex = lexema(file, aux, ftell(file)+1);
                 printf(" --- lexema: %s\n", lex);
+
+                struct Token t;
+                t.Lexema = lex;
+                t.NumLinea = num_lin;
+                t.Tipo = sim;
+                t.Valor = 0;
+                insertar(t);
             }
 
         }
@@ -310,13 +440,13 @@ int main(){
         caracter = (char)fgetc(file);
 
         if(caracter=='\n'){
-            num_lin++;
-            printf("Linea %d\n", num_lin);
-            caracter = (char)fgetc(file);
+            contarLinea(file);
         }
 
 
     }
+
+    imprimirlista(raiz);
 
     fclose(file);
 
